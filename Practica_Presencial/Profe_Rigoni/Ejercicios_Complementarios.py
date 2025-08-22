d1a54623906d504569544f820a0d7177474f8d44
ssqnty <santinogodoygaldeano@gmail.com>
2025-08-22 18:34:32 -0300

    .

--- Practica_Presencial/Profe_Rigoni/Ejercicios_Complementarios.py
+++ Practica_Presencial/Profe_Rigoni/Ejercicios_Complementarios.py
@@ -1,21 +1,22 @@
+#ej1
 print("Ej 1==" )
 numero1 = 6
 print(f"Valor de numero1 es {numero1} ")
 print("")
 
-
+#ej2
 print("Ej 2==" )
 numero2 = 2.5
 print(f"El valor de numero2 es {numero2}" )
 print("")
 
-
+#ej3
 print("Ej 3==" )
 suma = numero1 + numero2
 print(f"El resultado es {suma}")
 print("")
 
-
+#ej4
 print("Ej 4==" )
 resta = numero1 - numero2
 multiplicacion = numero1 * numero2
@@ -24,48 +25,49 @@ print("===Resultados:===" )
 print(f"Resta: {resta} \nMultiplicacion: {multiplicacion} \nDivision: { division}")
 print("")
 
-
+#ej5
 print("Ej 5==" )
 nombre = "Santino"
 print(f"Mi nombre es {nombre}")
 print("")
 
-
+#ej6
 print("Ej 6==" )
 precio = 500.45
 print(f"El precio es ${precio}")
 print("")
 
+#ej7
 print("Ej 7==" )
 descuento = 0.15
 print(f"El descuento es de {descuento}%")
 print("")
 
-
+#ej8
 print("Ej 8==" )
 precio_final = (precio * descuento) / 100
 print(f"El precio con 15% es {precio_final}")
 print("")
 
-
+#ej9
 print("Ej 9==" )
 cadena = "UTN"
 print(f"Elegi la palabra {cadena}")
 print("")
 
-
+#ej10
 print("Ej 10==" )
 longitud = len((cadena))
 print(f"La longitud de {cadena} es de {longitud}")
 print("")
 
-
+#ej11
 print("Ej 11==" )
 precio = int(8.99)
 print(f"El precio con decimales es 8.99 y convertido es {precio}")
 print("")
 
-
+#ej12
 print("Ej 12==" )
 nombre = "Santino"
 apellido = "Godoy Galdeano"
@@ -73,27 +75,27 @@ nombre_completo = print(f"{nombre} {apellido}. ")
 print(f"Mi nombre completo es {nombre_completo}" )
 print("")
 
-
+#ej13
 print("Ej 13==" )
 edad = (19 + 5 - 10)
 print(f"Mi edad(19) sumando 5 y restandole 10 es {edad} ")
 print("")
 
-
+#ej14
 print("Ej 14==" )
 altura = float(1.72)
 cuenta = (altura * 4) / 3
 print(f"Resultado de mi altura(1.72) x 4 / 3 es {cuenta} ")
 print("")
 
-
+#ej15
 print("Ej 15==" )
 mi_nombre = "SANTINO GODOY"
 nombre_minusculas = mi_nombre.lower()
 print(nombre_minusculas)
 print("")
 
-
+#ej16
 print("Ej 16==" )
 nombre_primer_letra = mi_nombre.capitalize()
 print(nombre_primer_letra )
